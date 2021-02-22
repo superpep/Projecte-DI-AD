8a01dd8d00aea51c1eebbfcfb5a173365e59f70d
Àlex <alexcrafteo1@outlook.es>
2021-02-22 08:23:19 +0100

    Eliminada la llibrería de criptografía ja que no s'ha d'encriptar res al servidor

--- server/lib/crypto.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-module.exports = function md5Encrypt(encryptString){
-    var hasher =  crypto.createHash("md5");
-    hasher.update(encryptString);
-    encryptString= hasher.digest('hex');
-    return encryptString;
-
-}
