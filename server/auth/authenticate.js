9230f2a2b53fe39241a1f01e20141af8e9c6ed8f
Àlex <alexcrafteo1@outlook.es>
2021-02-23 22:13:30 +0100

    Módul d'autenticació

---
+++ server/auth/authenticate.js
@@ -0,0 +1,43 @@
+const jwt = require('jsonwebtoken');
+
+const accessTokenSecret = '?;>+Z@^Jw{O~,2*h)TrV1$?0/t_lsb';
+const refreshTokenSecret = '*g">j`{yX,D1UI7)laqbNEWzEr26HO';
+const refreshTokens = [];
+
+function authenticateJWT(req, res, next){
+    // arrepleguem el JWT d'autorització
+    const authHeader = req.headers.authorization;
+    if (authHeader) { // si hi ha toquen
+        // recuperem el jwt
+        const token = authHeader.split(' ')[1]; jwt.verify(token, accessTokenSecret, (err, user) => {
+            if (err) { // Token incorrecte
+                return res.sendStatus(403); 
+            }
+    
+            // afegim a la petició les dades que venien en el jwt user
+            req.user = user;
+            // s'executa la segïuent funció, un cop s'ha fet el middleware
+            next();
+        });
+    } else { // no està. contestem directament al client amb un error 401 (unauthorized)
+            res.sendStatus(401);
+        }
+};
+
+function authorizeAlumne(req, res, next){
+    if(req.user.role == 'alumne'){
+        next()
+    } else {
+        return res.sendStatus(403); 
+    }
+}
+
+function authorizeProfe(req, res, next){
+    if(req.user.role == 'profe'){
+        next()
+    } else {
+        return res.sendStatus(403); 
+    }
+}
+
+module.exports = {accessTokenSecret, refreshTokenSecret, refreshTokens, authenticateJWT, authorizeAlumne, authorizeProfe}
