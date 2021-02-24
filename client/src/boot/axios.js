81150a3d007c4f8e4dc6d4f02b0bbd35cae0dd11
Àlex <alexcrafteo1@outlook.es>
2021-02-24 21:56:40 +0100

    Modificat axios

--- client/src/boot/axios.js
+++ client/src/boot/axios.js
@@ -2,3 +2,11 @@ import Vue from 'vue'
 import axios from 'axios'
 
 Vue.prototype.$axios = axios
+
+const api = axios.create({
+  baseURL: 'http://localhost:8000'
+})
+
+Vue.prototype.$api = api
+
+export { axios, api }
