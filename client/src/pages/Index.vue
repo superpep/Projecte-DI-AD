0e170b284b3834ee44a9b482980b282c7b4c69ec
Àlex <alexcrafteo1@outlook.es>
2021-02-24 21:57:18 +0100

    El login ja funciona

--- client/src/pages/Index.vue
+++ client/src/pages/Index.vue
@@ -16,7 +16,7 @@
             </q-form>
           </q-card-section>
           <q-card-actions class="q-px-md">
-            <q-btn unelevated color="secondary" size="lg" class="full-width" label="Login" />
+            <q-btn unelevated color="secondary" size="lg" @click="login" class="full-width" label="Login" />
           </q-card-actions>
           <q-card-section class="text-center q-pa-none">
             <p class="text-grey-6">No estàs registrat? <a href="#/register">Registra't</a></p>
@@ -26,14 +26,40 @@
     </div>
   </q-page>
 </template>
-
 <script>
+const crypto = require('crypto')
 export default {
+  preFetch ({ store, redirect }) {
+    if (store.state.showcase.user.token) { // SI JA ESTEM LOGUEJATS
+      redirect({ path: 'welcome' }) // REDIRECCIONEM A WELCOME
+    }
+  },
   name: 'PageIndex',
   data () {
     return {
       username: '',
       password: ''
+    }
+  },
+  methods: {
+    login () {
+      this.$store.dispatch('showcase/login', {
+        username: this.username,
+        password: crypto.createHash('md5').update(this.password).digest('hex')
+      })
+        .then(algo => {
+          this.$q.notify({
+            type: 'positive',
+            message: 'Login correcte'
+          })
+          this.$router.push('welcome')
+        })
+        .catch(err =>
+          this.$q.notify({
+            type: 'negative',
+            message: err
+          })
+        )
     }
   }
 }
