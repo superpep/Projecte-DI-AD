fd29dbd77208e96fb1ac447bfdc07f7419135979
Àlex <alexcrafteo1@outlook.es>
2021-02-24 21:57:30 +0100

    Nova pàgina de welcome

---
+++ client/src/pages/Welcome.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page class="bg-secondary window-height window-width row justify-center items-center">
+    <h1>Benvolgut, {{ username }}</h1>
+  </q-page>
+</template>
+<script>
+export default {
+  preFetch ({ store, redirect }) {
+    if (!store.state.showcase.user.token) { // SI NO HI HA TOKEN...
+      redirect({ path: '/' }) // REDIRECCIONEM A LOGIN JA QUE NO TÉ PERMISSOS PER A VEURE AQUESTA PÀGINA
+    }
+  },
+  name: 'Welcome',
+  computed: {
+    username: {
+      get () {
+        return this.$store.state.showcase.user.username
+      }
+    }
+  }
+}
+</script>
